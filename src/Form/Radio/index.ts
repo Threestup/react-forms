55ebf947ea4063ce31804f7774a2d8efdb9e237a
Sam Rae <samuelrae@gmail.com>
2017-05-10 14:16:16 +0100

    formatting

--- src/Form/Radio/index.ts
+++ src/Form/Radio/index.ts
@@ -1,32 +1,32 @@
 import { FormUpdateEvent } from '../.';
 
 export interface RadioValue {
-  label: string;
-  value: string;
+    label:string;
+    value:string;
 }
 
 export interface IRadio {
-  disabled: boolean;
-  name: string;
-  onClick: FormUpdateEvent<IRadio>;
-  selectedValue: string | undefined;
-  values: RadioValue[];
-  wrapperClassName: string;
+    disabled:boolean;
+    name:string;
+    onClick:FormUpdateEvent<IRadio>;
+    selectedValue:string | undefined;
+    values:RadioValue[];
+    wrapperClassName:string;
 }
 
 export type IRadioPartial = Partial<IRadio>;
 
-const defaultRadioConfig: IRadio = {
-  disabled: false,
-  name: '',
-  onClick: (newRadio: IRadio) => null,
-  selectedValue: undefined,
-  values: [],
-  wrapperClassName: '',
+const defaultRadioConfig:IRadio = {
+    disabled: false,
+    name: '',
+    onClick: (newRadio:IRadio) => null,
+    selectedValue: undefined,
+    values: [],
+    wrapperClassName: '',
 };
 
-export function configureRadio (override: IRadioPartial = {}): IRadio {
-  return Object.assign({}, defaultRadioConfig, override);
+export function configureRadio (override:IRadioPartial = {}):IRadio {
+    return Object.assign({}, defaultRadioConfig, override);
 };
 
 export default configureRadio;
