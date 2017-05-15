bb0b071cd5edab2269ac191d5aeb38df2c646955
Will McVay <willmcvay@gmail.com>
2017-05-15 10:01:59 +0100

    Adds export for radios

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,6 @@ export { SelectComponent } from './Form/Select/El';
 export { IToggle, configureToggle } from './Form/Toggle';
 export { ToggleComponent } from './Form/Toggle/El';
 export { ICheckbox, configureCheckbox } from './Form/Checkbox';
-export { CheckboxComponent } from './Form/Checkbox/El'
+export { CheckboxComponent } from './Form/Checkbox/El';
+export { IRadio, configureRadio } from './Form/Radio';
+export { RadioComponent } from './Form/Radio/El';
